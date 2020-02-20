0c6eeb091001e6f3dad45c1d8372273144c1e6bf
Mike Warren <mikeywarren89@gmail.com>
2020-02-20 13:49:07 +0900

    seeded cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
-require "open-uri"
+# require "open-uri"
 
-Ingredient.destroy_all
+# Ingredient.destroy_all
 
-json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+# json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 
-items = JSON.parse(json)
+# items = JSON.parse(json)
 
-items["drinks"].each do |ingredient|
-  Ingredient.create!(name: ingredient["strIngredient1"])
-end
+# items["drinks"].each do |ingredient|
+#   Ingredient.create!(name: ingredient["strIngredient1"])
+# end
+
+Cocktail.create(name: "Gin and Tonic")
+Cocktail.create(name: "Dark and Stormy")
+Cocktail.create(name: "Tequila Sunrise")
+Cocktail.create(name: "Old Fashioned")
+Cocktail.create(name: "Le Wagon")
