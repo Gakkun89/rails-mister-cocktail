60fe06b8081e12a2bb1cefa9ae31d943b2a15cf9
Mike Warren <mikeywarren89@gmail.com>
2020-02-21 11:05:48 +0900

    added seed for ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 require "open-uri"
 
-# Ingredient.destroy_all
+Ingredient.destroy_all
 
-# json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
-
-# items = JSON.parse(json)
-
-# items["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient["strIngredient1"])
-# end
-
-Cocktail.destroy_all
-
-json = open("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=").read
+json = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
 
 items = JSON.parse(json)
 
-items["drinks"].each do |drink|
-  Cocktail.create!(name: drink["strDrink"])
+items["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
 end
+
+# Cocktail.destroy_all
+
+# json = open("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=").read
+
+# items = JSON.parse(json)
+
+# items["drinks"].each do |drink|
+#   Cocktail.create!(name: drink["strDrink"])
+# end
