e0e8065f6311a7ff91c4b7d0358e91b3fbfd9913
Mike Warren <mikeywarren89@gmail.com>
2020-02-21 16:01:39 +0900

    move select2 css to aplication

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
-import 'select2/dist/css/select2.css';
+// import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
 import { dropdown, dropdown1 } from '../components/dropdown';
 
